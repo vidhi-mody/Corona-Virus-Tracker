52d9524ee3a761b5f868486443db31fe83f7d6c5
Vidhi Mody <vidhimody98@gmail.com>
2020-04-25 01:43:56 +0530

    First Deployment Commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <h4><i>Stay home, stay safe!</i></h4>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Cards data={data} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
